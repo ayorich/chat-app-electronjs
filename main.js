ba439624ed7e519ffea33cb68c9ff30be1a445ab
ayorich <kayodele17@yahoo.com>
2021-01-23 09:38:22 +0100

    menu-electron-build

--- main.js
+++ main.js
@@ -1,12 +1,42 @@
-const { app, BrowserWindow, Notification } = require("electron");
+const {
+  app,
+  BrowserWindow,
+  Notification,
+  Menu,
+  Tray,
+  // nativeImage,
+} = require("electron");
 const { ipcMain } = require("electron/main");
 const path = require("path");
 const isDev = !app.isPackaged;
+
+const dockIcon = path.join(__dirname, "assets", "images", "react_app_logo.png");
+const trayIcon = path.join(__dirname, "assets", "images", "react_icon.png");
+
+// let win = null;
+function createSplashWindow() {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 200,
+    frame: false,
+    transparent: true,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      worldSafeExecuteJavaScript: true,
+    },
+  });
+  win.loadFile("splash.html");
+  return win;
+}
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    backgroundColor: "#6e707e",
+    icon: path.join(__dirname, "assets", "images", "react_icon.png"),
+    show: false,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
@@ -15,15 +45,51 @@ function createWindow() {
     },
   });
   win.loadFile("index.html");
-  isDev && win.webContents.openDevTools();
+  // isDev && win.webContents.openDevTools();
+  return win;
 }
 
 if (isDev) {
   require("electron-reload")(__dirname, {
     electron: path.join(__dirname, "node_modules", ".bin", "electron"),
+    hardResetMethod: "exit",
   });
+  // try {
+  //   require("electron-reloader")(module, {
+  //     debug: true,
+  //     watchRenderer: true,
+  //   });
+  // } catch (_) {
+  //   console.log("Error");
+  // }
 }
-app.whenReady().then(createWindow);
+
+if (process.platform === "darwin") {
+  app.dock.setIcon(dockIcon);
+}
+
+// let tray = null;
+app.whenReady().then(() => {
+  const template = require("./utils/Menu").createTemplate(app);
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  tray = new Tray(trayIcon);
+  tray.setContextMenu(menu);
+
+  const splash = createSplashWindow();
+  const mainWindow = createWindow();
+
+  mainWindow.once("ready-to-show", () => {
+    // splash.destroy();
+    // mainWindow.show();
+    setTimeout(() => {
+      splash.destroy();
+      mainWindow.show();
+    }, 3000);
+  });
+  // win.tray = new Tray(nativeImage.createFromPath(trayIcon));
+});
 
 ipcMain.on("notify", (_, message) => {
   new Notification({
